3f1c25a08030065284d86f0e498e6ab07fa1127b
ReyBos <andreybosiy@yandex.ru>
2020-04-07 23:32:05 +0300

    lesson 26

--- models/Category.php
+++ models/Category.php
@@ -2,18 +2,35 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
-use app\models\Product;
-
-class Category extends ActiveRecord
+class Category extends \yii\db\ActiveRecord
 {
     public static function tableName()
     {
         return 'category';
     }
-    
-    public function getProducts()
+
+    public function rules()
     {
-        return $this->hasMany(Product::className(), ['category_id' => 'id']);
+        return [
+            [['parent_id'], 'integer'],
+            [['name'], 'required'],
+            [['name', 'keywords', 'description'], 'string', 'max' => 255],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'id' => '№ категории',
+            'parent_id' => 'Родительская категория',
+            'name' => 'Название',
+            'keywords' => 'Ключевые слова',
+            'description' => 'Мета-описание',
+        ];
+    }
+    
+    public function getCategory() 
+    {
+        return $this->hasOne(Category::className(), ['id' => 'parent_id']);
     }
 }
