f47dc57a0c39ff514e1d26968a93f2803478fdbd
ReyBos <andreybosiy@yandex.ru>
2020-04-09 16:50:01 +0300

    lesson 28

--- models/Product.php
+++ models/Product.php
@@ -2,14 +2,61 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
-use app\models\Category;
-
-class Product  extends ActiveRecord
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property string $name
+ * @property string|null $content
+ * @property float $price
+ * @property string|null $keywords
+ * @property string|null $description
+ * @property string|null $img
+ * @property string $hit
+ * @property string $new
+ * @property string $sale
+ *
+ * @property OrderItems[] $orderItems
+ */
+class Product extends \yii\db\ActiveRecord
 {
     public static function tableName()
     {
         return 'product';
+    }
+
+    public function rules()
+    {
+        return [
+            [['category_id', 'name'], 'required'],
+            [['category_id'], 'integer'],
+            [['content', 'hit', 'new', 'sale'], 'string'],
+            [['price'], 'number'],
+            [['name', 'keywords', 'description', 'img'], 'string', 'max' => 255],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID товара',
+            'category_id' => 'Категория',
+            'name' => 'Наименование',
+            'content' => 'Контент',
+            'price' => 'Цена',
+            'keywords' => 'Ключевые слова',
+            'description' => 'Мета-описание',
+            'img' => 'Фото',
+            'hit' => 'Хит',
+            'new' => 'Новинка',
+            'sale' => 'Распродажа',
+        ];
+    }
+
+    public function getOrderItems()
+    {
+        return $this->hasMany(OrderItems::className(), ['product_id' => 'id']);
     }
     
     public function getCategory()
